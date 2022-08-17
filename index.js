b2d80fb798a703e925033cfafb3f94bb6b53dafd
MurituMaina <vincent.maina@student.moringaschool.com>
2022-08-17 22:18:52 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
-}
+  if(feet<400){
+    return 'This one is on me!';
+  }
+  else if (feet>2000 && feet <=2500){
+    return 'I will gladly take your thirty bucks.';
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+  }
+  else if(feet >2500){
+    return 'No can do.';
+  }
+}scuberGreetingForFeet(199);
 
-function switchOnCharmFromTip(){
+function ternaryCheckCity(cityName){
   // Write your code here!
+
+  let city = (cityName === "NYC") ?  'Ok, sounds good.' : 'No go.';
+    
+return city;
 }
+console.log(ternaryCheckCity("NYC"));
+console.log(ternaryCheckCity("Pittsburgh"));
+
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+if(tip === "generous"){
+  return "Thank you so much.";
+}
+else if(tip === "not as generous"){
+return "Thank you.";
+}
+else{
+  return "Bye.";
+}
+}
+switchOnCharmFromTip("generous")
